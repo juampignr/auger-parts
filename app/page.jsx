52139fc42a08c3c96ea4573ca1f183bf1fec5ff6
juampignr <juampi.gnr@gmail.com>
2024-08-02 11:06:09 -0300

    ...

--- app/page.jsx
+++ app/page.jsx
@@ -326,6 +326,7 @@ export default function Home() {
             placeholder="Qué componente o parte desea agregar?"
             className="max-w-lg"
             onSelectionChange={(change) => {
+              console.log(Object.values(change)[0]);
               ctx.setSelectedPart(Object.values(change)[0]);
             }}
           >
