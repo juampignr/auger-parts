fbf3fc762b36178a045e672073807f4be92cb65d
juampignr <juampi.gnr@gmail.com>
2024-08-23 15:50:20 -0300

    Deleting associated fields limits

--- app/api/associated/[field]/route.js
+++ app/api/associated/[field]/route.js
@@ -1,33 +1,31 @@
-import configuration from "/appConfig.json"
-import mysql from 'mysql2/promise';
+import configuration from "/appConfig.json";
+import mysql from "mysql2/promise";
 
-export const dynamic = 'force-dynamic' // defaults to auto
+export const dynamic = "force-dynamic"; // defaults to auto
 
-export async function GET(request,{ params }) {
+export async function GET(request, { params }) {
+  try {
+    const connection = await mysql.createConnection({
+      host: "db.auger.org.ar",
+      user: "mocca",
+      password: "sibyll",
+      database: "PMS",
+    });
 
-    try {
+    let [fields, metadata] = await connection.query(
+      `SELECT * FROM ${params.field}`,
+    );
 
-        const connection = await mysql.createConnection({
-            host: "db.auger.org.ar",
-            user: "mocca",
-            password: "sibyll",
-            database: 'PMS',
-        })
-
-        let [fields, metadata] = await connection.query(`SELECT * FROM ${params.field} LIMIT 1000`)
-        
-        if(!fields.length){
-
-            let [fields, metadata] = await connection.query(`SELECT * FROM ${params.field}Type LIMIT 1000`)
-
-        }
-
-        await connection.end()
-
-        return Response.json({"status":"ok",data:fields})
-
-    } catch (error) {
-            
-        return Response.json({"status":"error","error":error.stack})
+    if (!fields.length) {
+      let [fields, metadata] = await connection.query(
+        `SELECT * FROM ${params.field}Type`,
+      );
     }
+
+    await connection.end();
+
+    return Response.json({ status: "ok", data: fields });
+  } catch (error) {
+    return Response.json({ status: "error", error: error.stack });
+  }
 }
