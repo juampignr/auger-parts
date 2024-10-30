175855b55fac25f0b41ba7ea89ebc12bb7ad75c0
juampignr <juampi.gnr@gmail.com>
2024-10-30 11:36:30 -0300

    .

--- app/api/associated/[field]/route.js
+++ app/api/associated/[field]/route.js
@@ -12,16 +12,20 @@ export async function GET(request, { params }) {
       database: "PMS",
     });
 
+    let fields;
+    let metadata;
+
     if (params.field === "Status") {
-      let [fields, metadata] = await connection.query(
+      [fields, metadata] = await connection.query(
         `select ID,Name from Status union select ID,Name from SPStatus union select ID,Name from EkitStatus union select ID,Name from WaterBottleStatus union select ID,Name from SSDCoverStatus union select ID,Name from RadioStatus union select ID,Name from TestStatus union select ID,Name from SPMTStatus`,
       );
     } else {
-      let [describedFields, describedMetadata] = await connection.query(
+      const [describedFields, describedMetadata] = await connection.query(
         `describe ${params.field}`,
       );
 
-      let [fields, metadata] = await connection.query(
+      console.log(describedFields);
+      [fields, metadata] = await connection.query(
         `select ID,Name FROM ${params.field}`,
       );
 
